bf906858e84b9ff699bdacd574e6e4553bc3bb18
aromaric <aromaricadjei213@gmail.com>
2026-04-15 23:37:54 +0000

    Services in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component, computed, model, signal } from '@angular/core';
 import { CollectionItemCard } from './components/collection-item-card/collection-item-card';
-import { CollectionItem } from './models/collection-item';
 import { SearchBar } from './components/search-bar/search-bar';
 import { Collection } from './models/collection';
 import { CollectionService } from './services/collection-service';
+import { CollectionItem } from './models/collection-item';
 
 @Component({
   selector: 'app-root',
@@ -16,43 +16,36 @@ export class App {
 
   private collectionService = new CollectionService();
 
-  count = 0;
+  // count = 0;
   search = model('');
 
-  coin!: CollectionItem;
-  linx!: CollectionItem;
-  stamp!: CollectionItem;
+  // coin!: CollectionItem;
+  // linx!: CollectionItem;
+  // stamp!: CollectionItem;
 
   selectedCollection = signal<Collection | null>(null);
-  collectionItems = computed(() => {
-    const allItems = this.selectedCollection()?.items;
-    return allItems?.filter(item => item.name.toLowerCase().includes(
+  displayedItems = computed(() => {
+    const allItems = this.selectedCollection()?.items || [];
+    return allItems.filter(item => item.name.toLowerCase().includes(
       this.search().toLowerCase()
-    ))
+      )
+    )
   });
 
   constructor() {
-    this.coin = new CollectionItem();
-    this.coin.name = 'Pièce de 1972';
-    this.coin.description = 'Une Pièce de 1 dollar Americain.';
-    this.coin.rarity = 'Commune';
-    this.coin.image = 'img/coin1.png';
-    this.coin.price = 170;
+    const allCollections = this.collectionService.getAll();
+    if (allCollections.length > 0) {
+      this.selectedCollection.set(allCollections[0]);
+    }
+  }
 
-    this.linx = new CollectionItem();
-
-    this.stamp = new CollectionItem();
-    this.stamp.name = 'Vieux timbre';
-    this.stamp.description = 'Un vieux timbre';
-    this.stamp.rarity = 'Rare';
-    this.stamp.image = 'img/timbre1.png';
-    this.stamp.price = 555;
-
-    const defaultCollection = new Collection();
-    defaultCollection.title = 'Default Collection';
-    defaultCollection.items = [this.coin, this.linx, this.stamp];
-    this.selectedCollection.set(defaultCollection);
-
-    this.collectionService.hello();
+  addGenericItem(): void {
+    const collection = this.selectedCollection();
+    if (collection) {
+      const storedCollection = this.collectionService.addItem(
+        collection, new CollectionItem()
+      );
+      this.selectedCollection.set(storedCollection);
+    }
   }
 }
--- src/app/services/collection-service.ts
+++ src/app/services/collection-service.ts
@@ -1,11 +1,125 @@
 import { Injectable } from '@angular/core';
+import { Collection } from '../models/collection';
+import { CollectionItem } from '../models/collection-item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CollectionService {
+  private collections: Collection[] = [];
+  private currentId = 1;
+  private currentItemIndex: { [key: number]: number } = {};
 
-  hello(){
-    console.log('Hello CollectionService');
+  constructor() {
+    this.generateDummyData();
+  }
+
+  private generateDummyData() {
+    const coin = new CollectionItem();
+    coin.name = 'Pièce de 1972';
+    coin.description = 'Pièce de 1 dollar Americain.';
+    coin.rarity = 'Common';
+    coin.image = 'img/coin1.png';
+    coin.price = 170;
+
+    const stamp = new CollectionItem();
+    stamp.name = 'Timbre 1800';
+    stamp.description = 'Un très vieux timbre.';
+    stamp.rarity = 'Rarity';
+    stamp.image = 'img/timbre1.png';
+    stamp.price = 555;
+
+    const linx = new CollectionItem();
+
+    const defaultCollection = new Collection();
+    defaultCollection.title = 'Collection mix';
+
+    const storedCollection = this.add(defaultCollection);
+    this.addItem(storedCollection, coin);
+    this.addItem(storedCollection, linx);
+    this.addItem(storedCollection, stamp);
+  }
+
+  getAll(): Collection[] {
+    return this.collections.map(
+      collection => collection.copy());
+  }
+
+  get(collectionId: number) {
+    const storedCopy = this.collections.find(
+      collection => collection.id === collectionId);
+  }
+
+  add(collection: Omit<Collection, 'id' | 'items'>): Collection {
+    const storedCopy = collection.copy();
+    storedCopy.id = this.currentId;
+    this.collections.push(storedCopy);
+
+    this.currentItemIndex[storedCopy.id] = 1;
+    this.currentId++;
+
+    return storedCopy.copy();
+  }
+
+  update(collection: Omit<Collection, 'items'>): Collection | null {
+    const storedCopy = this.collections.find(
+      collection => collection.id === collection.id);
+
+    if (!storedCopy) return null;
+
+    Object.assign(storedCopy, collection);
+    return storedCopy.copy();
+  }
+
+  delete(collectionId: number): void {
+    this.collections = this.collections.filter(
+      collection => collection.id !== collectionId);
+  }
+
+  addItem(collection: Collection, item: CollectionItem): Collection | null {
+    const storedCollection = this.collections.find(
+      collection => collection.id === collection.id
+    );
+
+    if (!storedCollection) return null;
+
+    const storedItem = item.copy();
+    storedItem.id = this.currentItemIndex[collection.id]
+    storedCollection.items.push(storedItem);
+
+    this.currentItemIndex[collection.id]++;
+
+    return storedCollection.copy();
+  }
+
+  updateItem(collection : Collection, item: CollectionItem){
+    const storedCollection = this.collections.find(
+      storedCollection => storedCollection.id === collection.id
+    );
+
+    if (!storedCollection) return null;
+
+    const storedItemIndex = storedCollection.items.findIndex(
+      storedItem => storedItem.id === item.id
+    );
+
+    if (storedItemIndex === -1) return null;
+
+    storedCollection.items[storedItemIndex] = item.copy();
+    return storedCollection.copy();
+  }
+
+  deleteItem(collectionId: number, itemId:number): Collection | null {
+    const storedCollection = this.collections.find(
+      storedCollection => storedCollection.id === collectionId
+    );
+
+    if (!storedCollection) return null;
+
+    storedCollection.items = storedCollection.items.filter(
+      item => item.id === item.id
+    );
+
+    return storedCollection.copy();
   }
 }
