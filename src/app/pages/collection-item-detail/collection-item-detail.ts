1cafc3f9edd384dcd92f72e13b9ddae67af40216
aromaric <aromaricadjei213@gmail.com>
2026-04-16 13:56:24 +0000

    Navigation bases with routerLink function Angular

--- src/app/pages/collection-item-detail/collection-item-detail.ts
+++ src/app/pages/collection-item-detail/collection-item-detail.ts
@@ -1,28 +1,19 @@
 import { Component, inject, input} from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-collection-item-detail',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './collection-item-detail.html',
   styleUrl: './collection-item-detail.scss',
 })
-export class CollectionItemDetail{
-
+export class CollectionItemDetail {
   private readonly activedRoute = inject(ActivatedRoute);
-  private readonly  router = inject(Router);
+  private readonly router = inject(Router);
 
   itemId = input<number | null, string | null>(null, {
     alias: 'id',
-    transform: value => value ? parseInt(value) : null,
+    transform: (value) => (value ? parseInt(value) : null),
   });
-
-  next() {
-    const currentId = this.itemId();
-    if (currentId) {
-      const nextId = currentId + 1;
-      this.router.navigate(['item', nextId]);
-    }
-  }
 }
