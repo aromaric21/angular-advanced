f9ef2f00ee9d345508616f08830cbdd853a99a29
aromaric <aromaricadjei213@gmail.com>
2026-04-20 11:16:00 +0000

    Guardes/gardes in Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,18 @@ import { CollectionDetail } from './pages/collection-detail/collection-detail';
 import { CollectionItemDetail } from './pages/collection-item-detail/collection-item-detail';
 import { NotFound } from './pages/not-found/not-found';
 import { Login } from './pages/login/login';
+import { isLoggedInGuard } from './guards/is-logged-in/is-logged-in-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: CollectionDetail },
-  { path: 'item',
+  { path: 'home', component: CollectionDetail, canActivate: [isLoggedInGuard] },
+  {
+    path: 'item',
     children: [
-      {path: '',  component: CollectionItemDetail},
-      {path: ':id', component: CollectionItemDetail},
-    ]
+      { path: '', component: CollectionItemDetail, canActivate: [isLoggedInGuard]},
+      { path: ':id', component: CollectionItemDetail, canActivate: [isLoggedInGuard]},
+    ],
   },
-  {path: 'login', component: Login},
+  { path: 'login', component: Login },
   { path: '**', component: NotFound },
 ];
