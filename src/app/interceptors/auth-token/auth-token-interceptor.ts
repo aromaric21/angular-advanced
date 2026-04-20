873dedd280958805861c3c14d01fcfd519aa0ec1
aromaric <aromaricadjei213@gmail.com>
2026-04-20 10:50:40 +0000

    Login page and backend in Angular

---
+++ src/app/interceptors/auth-token/auth-token-interceptor.ts
@@ -0,0 +1,13 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authTokenInterceptor: HttpInterceptorFn = (
+  req, next) => {
+  const token = localStorage.getItem('TOKEN');
+  let requestToSend = req;
+
+  if (token) {
+    const headers = req.headers.set('Authorization', 'Bearer ' + token);
+    requestToSend = req.clone({headers: headers});
+  }
+  return next(requestToSend);
+};
